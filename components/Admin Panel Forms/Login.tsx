7fb9cf066ef90795184a82ff99720e5a659b0dad
skyrunner360 <mathurrishi31@gmail.com>
2023-02-19 20:45:06 +0530

    admin panel wip

---
+++ components/Admin Panel Forms/Login.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+
+const Login = () => {
+  return (
+    <>
+      <Box></Box>
+    </>
+  );
+};
+export default Login;
---
+++ pages/AdminPanel.tsx
@@ -0,0 +1,14 @@
+import Container from "@mui/material/Container";
+import Head from "next/head";
+
+const AdminPanel = () => {
+  return (
+    <>
+      <Head>
+        <title>Admin Panel</title>
+      </Head>
+      <Container></Container>
+    </>
+  );
+};
+export default AdminPanel;
