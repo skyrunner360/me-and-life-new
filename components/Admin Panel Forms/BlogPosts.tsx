4d7d4dbaccba67b618471bcf1fe4c64c1ba3dcb3
skyrunner360 <mathurrishi31@gmail.com>
2023-03-02 18:09:12 +0530

    blog post crud working

--- components/Admin Panel Forms/BlogPosts.tsx
+++ components/Admin Panel Forms/BlogPosts.tsx
@@ -2,9 +2,11 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import CircularProgress from "@mui/material/CircularProgress";
 import Typography from "@mui/material/Typography";
-import { useQuery } from "@tanstack/react-query";
-import { PostCard } from "./AdminHelperComponents";
-import { getBlogs } from "./AdminPanelLogic";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import { CommonModal } from "../common/CommonComponents";
+import { AddNewModalContent, PostCard } from "./AdminHelperComponents";
+import { addBlog, changeBlog, deleteBlog, getBlogs } from "./AdminPanelLogic";
 
 interface blogPostsResponse {
   _id: number;
@@ -24,10 +26,44 @@ interface blogPostsResponse {
   views: number;
 }
 const BlogPosts = () => {
+  const queryClient = useQueryClient();
+
   const blogsQuery = useQuery({
     queryKey: ["blogs"],
     queryFn: ({ queryKey }) => getBlogs().then((res) => res.data),
   });
+
+  const [openAddNewModal, setOpenAddNewModal] = useState(false);
+
+  const editBlogMutation = useMutation({
+    mutationFn: changeBlog,
+    onSuccess: () => queryClient.invalidateQueries(["blogs"]),
+  });
+
+  const addBlogMutation = useMutation({
+    mutationFn: addBlog,
+    onSuccess: () => queryClient.invalidateQueries(["blogs"]),
+  });
+
+  const editBlogFn = (findSlug: string, title: string, content: string, slug: string) => {
+    editBlogMutation.mutate({
+      slug: findSlug,
+      data: {
+        title,
+        content,
+        slug,
+      },
+    });
+  };
+
+  const deleteBlogFn = (slug: string) => {
+    deleteBlog({ slug })
+      .then((res) => {
+        queryClient.invalidateQueries(["blogs"]);
+      })
+      .catch((err) => console.log(`Error- ${err.message}`));
+  };
+
   const { data, isLoading, error } = blogsQuery;
   if (error) {
     return <pre>Something went Wrong {JSON.stringify(error)} </pre>;
@@ -47,16 +83,29 @@ const BlogPosts = () => {
               <PostCard
                 key={elem.slug}
                 elem={elem}
-                onEdit={() => console.log("Edit from Blogpost clicked")}
-                onDelete={() => console.log("Delete from Blog clicked")}
+                onEdit={(title: string, content: string, slug: string) =>
+                  editBlogFn(elem.slug, title, content, slug)
+                }
+                onDelete={() => deleteBlogFn(elem.slug)}
               />
             );
           })}
         </Box>
         <Box textAlign={"right"} p={1}>
-          <Button>Add new</Button>
+          <Button onClick={() => setOpenAddNewModal(true)}>Add new</Button>
         </Box>
       </Box>
+
+      <CommonModal open={openAddNewModal} onClose={() => setOpenAddNewModal(false)}>
+        <AddNewModalContent
+          onAdd={(title: string, content: string, slug: string) =>
+            addBlogMutation.mutate({
+              insert: { title, content, slug, author: "skyrunner", category: "blog" },
+            })
+          }
+          closeModal={() => setOpenAddNewModal(false)}
+        />
+      </CommonModal>
     </>
   );
 };
