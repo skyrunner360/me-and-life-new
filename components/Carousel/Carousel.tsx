51aaa0d39fdda96d0fe769ef391c1f0a616594fe
skyrunner360 <mathurrishi31@gmail.com>
2023-04-16 00:42:06 +0530

    carousel working

---
+++ components/Carousel/Carousel.tsx
@@ -0,0 +1,100 @@
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import MobileStepper from "@mui/material/MobileStepper";
+import { MouseEvent, useState } from "react";
+import SwipeableViews from "react-swipeable-views-react-18-fix";
+import { autoPlay } from "react-swipeable-views-utils";
+
+const Carousel = (props: any) => {
+  const { children } = props;
+  const [activeStep, setActiveStep] = useState(0);
+
+  const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+  const handleNext = (e: MouseEvent) => {
+    activeStep < children.length - 1 && setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    e.stopPropagation();
+  };
+
+  const handleBack = (e: MouseEvent) => {
+    activeStep >= 1 && setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    e.stopPropagation();
+  };
+
+  const handleStepChange = (step: number) => {
+    setActiveStep(step);
+  };
+
+  return (
+    <Box position={"relative"}>
+      <AutoPlaySwipeableViews index={activeStep} onChangeIndex={handleStepChange} enableMouseEvents>
+        {children}
+      </AutoPlaySwipeableViews>
+      <Box
+        sx={{
+          position: "absolute",
+          left: "5%",
+          top: "50%",
+          transform: "translateY(-50%)",
+        }}
+        onClick={handleBack}
+      >
+        <IconButton
+          disableFocusRipple
+          sx={{
+            width: "20px",
+            height: "20px",
+            background: "white",
+          }}
+          className={"PrdTheme"}
+          disabled={activeStep === 0}
+        >
+          <ArrowBackIosNewIcon sx={{ fontSize: "14px" }} />
+        </IconButton>
+      </Box>
+      <Box
+        sx={{
+          position: "absolute",
+          right: "5%",
+          top: "50%",
+          transform: "translateY(-50%)",
+        }}
+        onClick={handleNext}
+      >
+        <IconButton
+          sx={{
+            width: "18px",
+            height: "18px",
+            background: "white",
+          }}
+          className={"PrdTheme"}
+          disabled={activeStep === children.length - 1}
+        >
+          <ArrowForwardIosIcon sx={{ fontSize: "14px" }} />
+        </IconButton>
+      </Box>
+      <MobileStepper
+        position="static"
+        steps={children.length}
+        activeStep={activeStep}
+        sx={{
+          background: "transparent",
+          position: "absolute",
+          bottom: 0,
+          left: "50%",
+          transform: "translateX(-50%)",
+        }}
+        backButton={null}
+        nextButton={null}
+        className={"PrdTheme"}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      />
+    </Box>
+  );
+};
+
+export default Carousel;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import SideNavBar from "../components/SideNavBar";
-import { Box, Typography } from "@mui/material";
+import { Avatar, Box, Grid, Typography } from "@mui/material";
 import AnimatedText from "../components/AnimatedText";
+import Carousel from "../components/Carousel/Carousel";
 
 export default function Home() {
+  const carouselImages = [{ src: "menuIcon.jpg" }, { src: "mountains.svg" }];
   return (
     <div className={styles.container}>
       <Head>
@@ -12,8 +14,21 @@ export default function Home() {
         <meta name="description" content="Welcome to Rishi's blog - ME AND LIFE" />
       </Head>
       <SideNavBar />
-      <Box ml={10}>
-        <AnimatedText word="Test" />
+      <Box px={4}>
+        <Grid container>
+          <Grid item sm={6} xs={12}>
+            for card
+          </Grid>
+          <Grid item sm={6} xs={12}>
+            <Carousel>
+              {carouselImages.map((el) => {
+                return (
+                  <Avatar src={el.src} variant="square" sx={{ height: "100%", width: "100%" }} />
+                );
+              })}
+            </Carousel>
+          </Grid>
+        </Grid>
       </Box>
     </div>
   );
