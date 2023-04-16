e59bcfb38751c0bf29b718a1b7ec4984c5424ec3
skyrunner360 <mathurrishi31@gmail.com>
2023-04-16 21:31:34 +0530

    image added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import { Box, Grid } from "@mui/material";
 import Head from "next/head";
+import AnimatedBlob from "../components/AnimatedBlob";
+import AnimatedClippedText from "../components/AnimatedClippedText";
+import AnimatedText from "../components/AnimatedText";
 import SideHero from "../components/SideHero";
 import SideNavBar from "../components/SideNavBar";
 import styles from "../styles/Home.module.css";
-import AnimatedClippedText from "../components/AnimatedClippedText";
-import TrackingBlur from "../components/TrackingBlur";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -20,7 +22,17 @@ export default function Home() {
         </Box>
         <Grid container>
           <Grid item sm={6} xs={12}>
-            Work in progress
+            <Grid container>
+              <Grid item sm={5}>
+                <Box>
+                  <Box className={styles.heroImg}></Box>
+                </Box>
+              </Grid>
+              <Grid item sm={7} p={1}>
+                <AnimatedText word="Hi I'm Rishi Mathur" component={"h1"} />
+                <Box></Box>
+              </Grid>
+            </Grid>
           </Grid>
           <Grid item sm={6} xs={12}>
             <SideHero />
