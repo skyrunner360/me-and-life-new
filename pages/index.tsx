719ac5aef232ca6ad1e234cb07d622c2eaa60065
skyrunner360 <mathurrishi31@gmail.com>
2023-08-05 17:41:37 +0530

    design coming

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Stack, Typography } from "@mui/material";
 import Head from "next/head";
 import AnimatedBlob from "../components/AnimatedBlob";
 import AnimatedClippedText from "../components/AnimatedClippedText";
@@ -22,27 +22,44 @@ export default function Home() {
         <SideNavBar />
         <Box px={4} zIndex={2} position={"relative"}>
           <Box textAlign={"center"}>
-            {/* Test commit */}
-            <AnimatedClippedText fontSize="5rem" word="Welcome to my Blog" imgSrc="clipHero.jpg" />
+            <AnimatedClippedText fontSize="5rem" word="Me and Life" imgSrc="/clipHero.jpg" />
           </Box>
           <Grid container>
-            <Grid item sm={6} xs={12}>
-              <Grid container>
-                <Grid item sm={5} xs={12}>
-                  <Box className={styles.heroImg}></Box>
+            <Grid item sm={6} p={1} xs={12}>
+              <Grid container height={"100%"}>
+                <Grid item sm={4} xs={12} height={"50%"}>
+                  <Box position={"relative"} sx={{ height: "100%", width: "100%" }}>
+                    <Image src={"/ikbal 3.svg"} alt="Greet Avatar" fill />
+                  </Box>
                 </Grid>
-                <Grid item sm={7} p={1} xs={12}>
+                <Grid item sm={8} xs={12}>
                   <ClippedText
-                    word="Hi! I'm Rishi Mathur "
+                    word="Welcome to My Blog!"
                     fontSize="2rem"
-                    imgSrc="carousel1_bkp.jpg"
+                    imgSrc="/carousel1_bkp.jpg"
                   />
-                  <Box></Box>
+                  <Box py={1}>
+                    <Typography variant="h6" fontWeight={500}>
+                      Hi! 👋 I&apos;m Rishi Mathur
+                    </Typography>
+                  </Box>
+                  <Box>
+                    <Typography>
+                      Welcome to the small world of this Introvert. Here I&apos;ll be posting some
+                      personal life updates, Some Tech Content and Some Original pieces of writing
+                      or whatever comes to my mind.
+                    </Typography>
+                  </Box>
+                  <Box py={1}>
+                    <Typography>Feel free to explore this blog.</Typography>
+                  </Box>
                 </Grid>
               </Grid>
             </Grid>
-            <Grid item sm={6} xs={12}>
-              <SideHero />
+            <Grid item sm={6} xs={12} p={2}>
+              <Box position={"relative"} sx={{ height: "35rem", width: "35rem" }}>
+                <Image src={"/Other 03.svg"} alt="Other Icon" fill />
+              </Box>
             </Grid>
           </Grid>
         </Box>
