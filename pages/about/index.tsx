5db85e603c6195c51491ceafe7a2101436731cf0
skyrunner360 <mathurrishi31@gmail.com>
2023-08-05 19:23:06 +0530

    about init

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import Box from "@mui/material/Box";
+import ClippedText from "../../components/ClippedText";
+import Head from "next/head";
 
 const index = () => {
   return (
     <>
-      <Layout>about</Layout>
+      <Head>
+        <title>Me and Life - About</title>
+        <meta name="description" content="About Rishi Mathur" />
+      </Head>
+      <Layout>
+        <Box>
+          <Box textAlign={"center"}>
+            <ClippedText word="test" fontSize="5rem" imgSrc="/carousel1_bkp.jpg" />
+          </Box>
+        </Box>
+      </Layout>
     </>
   );
 };
