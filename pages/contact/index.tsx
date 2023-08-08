877410c0b3b111e91df5f5d038b56e66611326c4
skyrunner360 <mathurrishi31@gmail.com>
2023-08-08 23:16:51 +0530

    mobile fix

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -15,7 +15,9 @@ const index = () => {
       <Layout>
         <Box textAlign={"center"}>
           <AnimatedClippedText fontSize="5rem" word="Contact Me" imgSrc="/clipHero.jpg" />
-          <AboutCard />
+          <Box sx={{ mx: "4rem" }}>
+            <AboutCard />
+          </Box>
         </Box>
       </Layout>
     </>
